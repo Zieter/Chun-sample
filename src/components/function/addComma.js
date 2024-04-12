8231b388fec24caab6b7c8173d87c1594c68b7fc
https://github.com/Zieter/React-project.git <chunchen0118@gmail.com>
2024-04-12 21:15:05 +0800

    update

--- src/components/function/addComma.js
+++ src/components/function/addComma.js
@@ -3,8 +3,12 @@ export const addComma = (price) => {
     const commaReg = /\B(?<!\.\d*)(?=(\d{3})+(?!\d))/g;
     const removeComma = priceStr.replace(/,/g, '');
     const priceComma = removeComma.replace(commaReg, ',');
+    if (priceStr === '0') {
+        return '0';
+    }
+
     if (priceStr.charAt(0) === '0' && priceStr.charAt(1) !== '.') {
-        return '';
+        return priceStr.charAt(1);
     }
 
     return priceComma;
